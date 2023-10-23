56f3c172c3097b6bf38851fa6410c09000fdfba2
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-23 15:37:30 +0200

    milestone 0

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS :
+// Organizzare i singoli membri in card/schede
+// Aggiungere un nuovo membro al team
+// Consigli del giorno:
+// Ragioniamo come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice.
+// E ricordiamoci che console.log() è nostro amico!
+
+
+
+
+// milestone 0
+const ourTeam = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        picture: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        picture: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        picture: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        picture: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name : 'Scott Estrada',
+        role : 'Developer',
+        picture: 'scott-estrada-developer.jpg'
+    },
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        picture: 'barbara-ramos-graphic-designer.jpg'
+    }
+
+]
+
+
+
+// milestone 1
+
+// for (let i = 0; i<ourTeam.lenght; i++){
+
+//     console.log(ourTeam[i].nome)
+//     console.log(ourTeam[i].role)
+//     console.log(ourTeam[i].picture)
+
+// }
+// milestone 2
+// ciclo for           
+
+
+
+// bonus 
+// 3 input collegati ad un bottone 
+
+
+// */
